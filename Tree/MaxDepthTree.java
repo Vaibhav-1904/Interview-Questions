34d1b41dff2a4da8df9eed5bff491f3d7573974b
Vaibhav Patel <vaibhav6644@gmail.com>
2021-09-06 20:13:33 +0530

    Depth of Tree

---
+++ Tree/MaxDepthTree.java
@@ -0,0 +1,31 @@
+package ImportantQ.Tree;
+
+public class MaxDepthTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // One Approach is to use Level Order (BFS) to find max depth, S-> O(n)
+
+    //recursive auxiliary space T->O(n)
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+
+        return 1 + Math.max(l, r);
+    }
+}
